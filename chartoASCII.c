0cde9e092f3c6346a50bca125951a1bf80494c1b
Piyush <44377484+piyush19198@users.noreply.github.com>
2021-05-20 14:46:23 +0530

    Update chartoASCII.c

--- chartoASCII.c
+++ chartoASCII.c
@@ -4,13 +4,16 @@ the character for user entered ASCII value.
 
 #include<stdio.h>
 
-int main()
+int main(void)
 {
     char val;
     printf("Enter the Value to convert into ASCII : ");
     scanf("%c",&val);
 
-    printf("ASCII value is : %d",val);
+    printf("ASCII value of %c is : %d \n",val,val);
+    printf("Decimal Value of ASCII of %c is : %d \n", val,val);
+    printf("Octal Value of ASCII of %c is : %o \n",val,val);
+    printf("Hexadecimal Value of ASCII of %c is : %x", val,val);
     
     return 0;
 }
