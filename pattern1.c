23e279f6d9433eef31897fba4abeb712e2bdbba6
Piyush <44377484+piyush19198@users.noreply.github.com>
2021-05-20 13:54:59 +0530

    Create pattern1.c

---
+++ pattern1.c
@@ -0,0 +1,19 @@
+/*
+Write a program to print following pattern.
+a. Using multiple printf statements
+b. Using single printf statement
+*
+**
+***
+****
+*****
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("*\n**\n***\n****\n*****");
+
+    return 0;
+}
